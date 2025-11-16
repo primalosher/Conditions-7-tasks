7d1986640c2fb862fc6789c85fbcd47b8a75f8f2
osher <osherisak@gmail.com>
2025-11-16 09:57:45 +0200

    Did tasks 5-9

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace ConsoleApp10
             int usage;
             double price;
             bool isBusiness;
-            usage =int.Parse(Console.ReadLine());           
+            usage = int.Parse(Console.ReadLine());
             if (usage > 0 && usage < 300)
                 price = 0.5 * usage;
             else if (usage > 301 && usage < 600)
@@ -41,9 +41,9 @@ namespace ConsoleApp10
             Console.WriteLine("Is it business");
             isBusiness = bool.Parse(Console.ReadLine());
             if (isBusiness == true)
-                price = price+price * 0.15;
+                price = price + price * 0.15;
             else
-                price = price+price * 0.05;
+                price = price + price * 0.05;
             Console.WriteLine("The price is: " + price);
 
 
@@ -68,8 +68,6 @@ namespace ConsoleApp10
                 pensioner = bool.Parse(Console.ReadLine());
                 if (pensioner == true)
                     cardPrice = cardPrice - cardPrice * 0.30;
-                else
-                    cardPrice = cardPrice;
             }
             Console.WriteLine("The final price is: " + cardPrice);
         }
@@ -86,7 +84,6 @@ namespace ConsoleApp10
                 bool school = bool.Parse(Console.ReadLine());
                 if (school == true)
                     price = price - price * 0.20;
-                else price = price;
             }
             else if (age > 19 && age < 60)
             {
@@ -95,8 +92,6 @@ namespace ConsoleApp10
                 bool vip = bool.Parse(Console.ReadLine());
                 if (vip == true)
                     price = price + price * 0.30;
-                else
-                    price = price;
             }
             else
             {
@@ -105,15 +100,133 @@ namespace ConsoleApp10
                 bool IDF = bool.Parse(Console.ReadLine());
                 if (IDF == true)
                     price = price - price * 0.15;
-                else
-                    price = price;
             }
             Console.WriteLine("The final price is: " + price);
 
         }
+        static void Task5()
+        {
+            string roomType;
+            double price;
+            bool weekEnd, clubMember;
+            Console.WriteLine("Is the room normal or suite");
+            roomType = Console.ReadLine();
+            if (roomType == "normal")
+                price = 400;
+            else 
+                price = 700;
+            Console.WriteLine("Is the order in the weekend");
+            weekEnd = bool.Parse(Console.ReadLine());
+            if (weekEnd == true)
+                price = price + price * 0.1;
+            Console.WriteLine("Are you a club member");
+            clubMember = bool.Parse(Console.ReadLine());
+            if (clubMember == true)
+                price = price - price * 0.15;
+            Console.WriteLine("The price is: " + price);
+        }
+        static void Task6()
+        {
+            int age;
+            double price;
+            Console.WriteLine("Enter your age:");
+            age = int.Parse(Console.ReadLine());
+            if (age > 0 && age < 12)
+            {
+                price = 50;
+                Console.WriteLine("Is it shabat");
+                bool shabat = bool.Parse(Console.ReadLine());
+                if (shabat == true)
+                    price = price + 10;
+            }
+            else if (age > 13 && age < 17)
+            {
+                price = 70;
+                Console.WriteLine("club member?");
+                bool clubMember = bool.Parse(Console.ReadLine());
+                if (clubMember == true)
+                    price = price - price * 0.20;
+            }
+            else
+            {
+                price = 100;
+                Console.WriteLine("VIP member?");
+                bool isVIP = bool.Parse(Console.ReadLine());
+                if (isVIP == true)
+                    price = price + price * 0.30;
+            }
+            Console.WriteLine("The price is: " + price);
+
+
+        }
+        static void Task7()
+        {
+            int age;
+            double price;
+            Console.WriteLine("Enter your age");
+            age = int.Parse(Console.ReadLine());
+            if (age <= 24)
+            {
+                price = 600;
+                Console.WriteLine("Sport car?");
+                bool isSport = bool.Parse(Console.ReadLine());
+                if (isSport == true)
+                    price = price + price * 0.25;
+            }
+            else if (age >= 25 && age <= 50)
+            {
+                price = 450;
+                Console.WriteLine("Accidant last year?");
+                bool accidantLastYear = bool.Parse(Console.ReadLine());
+                if (accidantLastYear == true)
+                    price = price + price * 0.20;
+            }
+            else
+            {
+                price = 400;
+                Console.WriteLine("Is the car normal");
+                bool carNormal = bool.Parse(Console.ReadLine());
+                if (carNormal == true)
+                    price = price - price * 0.1;
+            }
+            Console.WriteLine("The price is: " + price);
+                
+                
+        }
+        static void Task8()
+        {
+            Console.WriteLine("Enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                if (num % 4 == 0)
+                    Console.WriteLine("The number can be divided by 4");
+                else
+                    Console.WriteLine("Even but cant be divided by 4");
+            }
+            else 
+                Console.WriteLine("The number is odd");
+        }
+        static void Task9()
+        {
+            bool admin;
+            Console.WriteLine("Are you admin");
+            admin = bool.Parse(Console.ReadLine());
+            if (admin == true)
+            {
+                Console.WriteLine("What is the password:");
+                int password = int.Parse(Console.ReadLine());
+                if (password == 1234)
+                    Console.WriteLine("Acsess approved");
+                else
+                    Console.WriteLine("Wrong password");
+            }
+            else 
+                Console.WriteLine("User doesnt exist");
+        }
         static void Main(string[] args)
         {
-           
+         
         }
     }
 }
